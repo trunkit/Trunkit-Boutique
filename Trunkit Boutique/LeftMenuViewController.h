983f6c4ab02ca0d707ebb26225a3cd4197b29fa4
Frank Le Grand <amorican@mac.com>
2014-08-29 21:29:34 -0600

    UI work on menu to look like the design

--- Trunkit Boutique/LeftMenuViewController.h
+++ Trunkit Boutique/LeftMenuViewController.h
@@ -15,4 +15,14 @@
 @property (strong, nonatomic) IBOutlet UILabel *termsLabel;
 @property (strong, nonatomic) IBOutlet UILabel *signoutLabel;
 
+@property (strong, nonatomic) IBOutlet UIImageView *suppliedMenuImageView;
+@property (strong, nonatomic) IBOutlet UIImageView *helpMenuImageView;
+@property (strong, nonatomic) IBOutlet UIImageView *termsMenuImageView;
+@property (strong, nonatomic) IBOutlet UIImageView *signoutMenuImageView;
+
+@property (strong, nonatomic) IBOutlet UIView *suppliedMenuFrameView;
+@property (strong, nonatomic) IBOutlet UIView *helpMenuFrameView;
+@property (strong, nonatomic) IBOutlet UIView *termsMenuFrameView;
+@property (strong, nonatomic) IBOutlet UIView *signoutMenuFrameView;
+
 @end
--- Trunkit Boutique/LeftMenuViewController.m
+++ Trunkit Boutique/LeftMenuViewController.m
@@ -9,9 +9,96 @@
 #import "LeftMenuViewController.h"
 #import "UILabel+UILabel_TKExtensions.h"
 
+
+@implementation UIImage (NegativeImage)
+
+- (UIImage *)negativeImage
+{
+    // get width and height as integers, since we'll be using them as
+    // array subscripts, etc, and this'll save a whole lot of casting
+    CGSize size = self.size;
+    int width = size.width;
+    int height = size.height;
+    
+    // Create a suitable RGB+alpha bitmap context in BGRA colour space
+    CGColorSpaceRef colourSpace = CGColorSpaceCreateDeviceRGB();
+    unsigned char *memoryPool = (unsigned char *)calloc(width*height*4, 1);
+    CGContextRef context = CGBitmapContextCreate(memoryPool, width, height, 8, width * 4, colourSpace, kCGBitmapByteOrder32Big | kCGImageAlphaPremultipliedLast);
+    CGColorSpaceRelease(colourSpace);
+    
+    // draw the current image to the newly created context
+    CGContextDrawImage(context, CGRectMake(0, 0, width, height), [self CGImage]);
+    
+    // run through every pixel, a scan line at a time...
+    for(int y = 0; y < height; y++)
+    {
+        // get a pointer to the start of this scan line
+        unsigned char *linePointer = &memoryPool[y * width * 4];
+        
+        // step through the pixels one by one...
+        for(int x = 0; x < width; x++)
+        {
+            // get RGB values. We're dealing with premultiplied alpha
+            // here, so we need to divide by the alpha channel (if it
+            // isn't zero, of course) to get uninflected RGB. We
+            // multiply by 255 to keep precision while still using
+            // integers
+            int r, g, b;
+            if(linePointer[3])
+            {
+                r = linePointer[0] * 255 / linePointer[3];
+                g = linePointer[1] * 255 / linePointer[3];
+                b = linePointer[2] * 255 / linePointer[3];
+            }
+            else
+            r = g = b = 0;
+            
+            // perform the colour inversion
+            r = 255 - r;
+            g = 255 - g;
+            b = 255 - b;
+            
+            // multiply by alpha again, divide by 255 to undo the
+            // scaling before, store the new values and advance
+            // the pointer we're reading pixel data from
+            linePointer[0] = r * linePointer[3] / 255;
+            linePointer[1] = g * linePointer[3] / 255;
+            linePointer[2] = b * linePointer[3] / 255;
+            linePointer += 4;
+        }
+    }
+    
+    // get a CG image from the context, wrap that into a
+    // UIImage
+    CGImageRef cgImage = CGBitmapContextCreateImage(context);
+    UIImage *returnImage = [UIImage imageWithCGImage:cgImage];
+    
+    // clean up
+    CGImageRelease(cgImage);
+    CGContextRelease(context);
+    free(memoryPool);
+    
+    // and return
+    return returnImage;
+}
+
+@end
+
+
+
 @interface LeftMenuViewController ()
 
 @property (strong, nonatomic) UITableView *myTableView;
+
+//@property (strong, nonatomic) UIImage *suppliedImage;
+//@property (strong, nonatomic) UIImage *helpImage;
+//@property (strong, nonatomic) UIImage *termsImage;
+//@property (strong, nonatomic) UIImage *signoutImage;
+@property (strong, nonatomic) UIImage *suppliedInvertedImage;
+@property (strong, nonatomic) UIImage *helpInvertedImage;
+@property (strong, nonatomic) UIImage *termsInvertedImage;
+@property (strong, nonatomic) UIImage *signoutInvertedImage;
+
 
 @end
 
@@ -44,6 +131,11 @@
         // The device is an iPhone or iPod touch.
         [self setFixedStatusBar];
     }
+    
+    self.suppliedInvertedImage = [[UIImage imageNamed:@"hangerMenuItemImage"] negativeImage];
+    self.helpInvertedImage = [[UIImage imageNamed:@"helpMenuItemImage"] negativeImage];
+    self.termsInvertedImage = [[UIImage imageNamed:@"termsMenuItemImage"] negativeImage];
+    self.signoutInvertedImage = [[UIImage imageNamed:@"signOutMenuImteImage"] negativeImage];
 }
 
 - (void)setFixedStatusBar
@@ -57,6 +149,31 @@
     UIView *statusBarView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, MAX(self.view.frame.size.width,self.view.frame.size.height), 20)];
     statusBarView.backgroundColor = [UIColor blackColor];
     [self.view addSubview:statusBarView];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *) indexPath
+{
+    UITableViewCell *cell = [super tableView:tableView cellForRowAtIndexPath:indexPath];
+    [cell setSelectionStyle:UITableViewCellSelectionStyleNone];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSInteger index = [indexPath indexAtPosition:1];
+    
+    self.suppliedMenuFrameView.backgroundColor = (index == 0) ? [UIColor blackColor] : [UIColor whiteColor];
+    self.helpMenuFrameView.backgroundColor = (index == 1) ? [UIColor blackColor] : [UIColor whiteColor];
+    self.termsMenuFrameView.backgroundColor = (index == 2) ? [UIColor blackColor] : [UIColor whiteColor];
+    self.signoutMenuFrameView.backgroundColor = (index == 3) ? [UIColor blackColor] : [UIColor whiteColor];
+
+    self.suppliedMenuImageView.image = (index == 0) ? self.suppliedInvertedImage : [UIImage imageNamed:@"hangerMenuItemImage"];
+    self.helpMenuImageView.image = (index == 1) ? self.helpInvertedImage : [UIImage imageNamed:@"helpMenuItemImage"];
+    self.termsMenuImageView.image = (index == 2) ? self.termsInvertedImage : [UIImage imageNamed:@"termsMenuItemImage"];
+    self.signoutMenuImageView.image = (index == 3) ? self.signoutInvertedImage : [UIImage imageNamed:@"signOutMenuImteImage"];
+    
+    
+    [super tableView:tableView didSelectRowAtIndexPath:indexPath];
 }
 
 @end
